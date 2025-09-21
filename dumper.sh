ab327968eb5abe0a969394c5729f62c8cc0cbd70
VanVuong41429 <vanvuong41429@gmail.com>
2025-09-21 17:51:20 +0700

    Update dumper.sh

--- dumper.sh
+++ dumper.sh
@@ -462,7 +462,7 @@ elif ${BIN_7ZZ} l -ba "${FILEPATH}" | grep rawprogram || [[ $(find "${TMPDIR}" -
 				if [[ -f "$partition.raw.img" ]]; then
 					mv "$partition.raw.img" "$partition.img"
 				else
-					rawprogramsfile=$(grep -rlw $partition rawprogram*.xml)
+					rawprogramsfile=$(grep -rlw $partition *rawprogram*)
 					"${PACKSPARSEIMG}" -t $partition -x $rawprogramsfile > ${TMPDIR}/extract.log
 					mv "$partition.raw" "$partition.img"
 				fi
