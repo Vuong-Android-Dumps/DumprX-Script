280189074ab029f37ce3bf1609a0cd6dc0a1270f
VanVuong41429 <vanvuong41429@gmail.com>
2026-03-21 10:11:49 +0700

    Improve error handling in dumper.sh for UPDATE.APP
    Refactor uv run command to handle errors more gracefully.

--- dumper.sh
+++ dumper.sh
@@ -676,11 +676,12 @@ elif ${BIN_7ZZ} l -ba "${FILEPATH}" | grep -q "UPDATE.APP" 2>/dev/null || [[ $(f
 	printf "Huawei UPDATE.APP Detected\n"
 	[[ -f "${FILEPATH}" ]] && ${BIN_7ZZ} x "${FILEPATH}" UPDATE.APP 2>/dev/null >> "${TMPDIR}"/zip.log
 	find "${TMPDIR}" -type f -name "UPDATE.APP" -exec mv {} . \;
-	uv run -q "${SPLITUAPP}" -f "UPDATE.APP" -l super preas preavs || (
+	uv run -q "${SPLITUAPP}" -f "UPDATE.APP" -l || (
 	for partition in ${PARTITIONS}; do
 		uv run -q "${SPLITUAPP}" -f "UPDATE.APP" -l "${partition/.img/}" || printf "%s not found in UPDATE.APP\n" "${partition}"
 	done )
 	find output/ -type f -name "*.img" -exec mv {} . \;	# Partitions Are Extracted In "output" Folder
+	"${SIMG2IMG}" super*.img super.img.raw 2>/dev/null && rm super*.img
 	if [[ -f super.img ]]; then
 		printf "Creating super.img.raw ...\n"
 		"${SIMG2IMG}" super.img super_* super.img.raw 2>/dev/null
