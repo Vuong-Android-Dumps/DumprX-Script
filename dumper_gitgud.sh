af39677c5fb846af2a5cb485dd21e63472e8471f
VanVuong41429 <vanvuong41429@gmail.com>
2026-06-05 19:55:11 +0700

    Fix git rm command to use --cached option

--- dumper_gitgud.sh
+++ dumper_gitgud.sh
@@ -1314,7 +1314,7 @@ commit_and_push(){
 
     echo "Adding extras..."
 	git add .
-	find . -path './.git' -prune -o -type f -size +100M -exec git rm -cached {} \;
+	find . -path './.git' -prune -o -type f -size +100M -exec git rm --cached {} \;
 	echo "Commiting extras..."
 	git commit -sm "Add extras for ${description}" > /dev/null 2>&1
 	git push -u origin "${branch}" || git push -u origin "${branch}" || git push -u origin "${branch}"
